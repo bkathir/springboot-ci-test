8c48e045d4f53eb2dc72596073e42e8084b17ea1
Kathiravan <kathiravan@BCTUS-K7V2VJ252Q.local>
2026-02-23 22:56:13 -0500

    First commit

---
+++ src/main/java/com/kathir/citest/SpringbootCiTestApplication.java
@@ -0,0 +1,13 @@
+package com.kathir.citest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCiTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kathir/citest/SpringbootCiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kathir.citest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
